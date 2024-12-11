1711152d2e59597c5543546a07fc2aa715f1d813
Robson Pereira Martins <profrobson.p.martins2023@gmail.com>
2024-12-11 10:22:24 -0300

    first commit

--- src/components/Destaques/Destaques.jsx
+++ src/components/Destaques/Destaques.jsx
@@ -9,6 +9,7 @@ const Destaques = () => {
         <p>Parabéns aos alunos que se destacaram nas competições acadêmicas!</p>
       </div>
       <div className="destaques-cards">
+        {/* 1 */}
         <div className="destaque-card">
           <img
             src="https://via.placeholder.com/300x200"
@@ -16,7 +17,9 @@ const Destaques = () => {
             className="destaque-image"
           />
           <div className="destaque-info">
-            <h3>Olimpíada de Matemática</h3>
+            <h3>
+              <i className="fa-solid fa-medal medalha-ouro"></i> Olimpíada de Matemática
+            </h3>
             <p>
               Parabéns ao aluno João Silva, que conquistou a medalha de ouro na
               Olimpíada Brasileira de Matemática (OBMEP), destacando-se pelo seu
@@ -24,6 +27,7 @@ const Destaques = () => {
             </p>
           </div>
         </div>
+        {/* 2 */}
         <div className="destaque-card">
           <img
             src="https://via.placeholder.com/300x200"
@@ -31,7 +35,9 @@ const Destaques = () => {
             className="destaque-image"
           />
           <div className="destaque-info">
-            <h3>Olimpíada de Astronomia</h3>
+            <h3>
+              <i className="fa-solid fa-medal medalha-prata"></i> Olimpíada de Astronomia
+            </h3>
             <p>
               Maria Souza foi a grande vencedora da Olimpíada Brasileira de
               Astronomia e Astronáutica, representando nossa escola com grande
@@ -39,6 +45,7 @@ const Destaques = () => {
             </p>
           </div>
         </div>
+        {/* 3 */}
         <div className="destaque-card">
           <img
             src="https://via.placeholder.com/300x200"
@@ -46,7 +53,9 @@ const Destaques = () => {
             className="destaque-image"
           />
           <div className="destaque-info">
-            <h3>Olimpíada de Redação</h3>
+            <h3>
+              <i className="fa-solid fa-medal medalha-bronze"></i> Olimpíada de Redação
+            </h3>
             <p>
               A aluna Ana Costa conquistou o primeiro lugar na Olimpíada Nacional
               de Redação, com seu texto premiado abordando temas de relevância
@@ -54,6 +63,64 @@ const Destaques = () => {
             </p>
           </div>
         </div>
+
+        {/* 4 */}
+
+        <div className="destaque-card">
+          <img
+            src="https://via.placeholder.com/300x200"
+            alt="Olimpíada de Matemática"
+            className="destaque-image"
+          />
+          <div className="destaque-info">
+            <h3>
+              <i className="fa-solid fa-medal medalha-ouro"></i> Olimpíada de Matemática
+            </h3>
+            <p>
+              Parabéns ao aluno João Silva, que conquistou a medalha de ouro na
+              Olimpíada Brasileira de Matemática (OBMEP), destacando-se pelo seu
+              talento e dedicação aos estudos.
+            </p>
+          </div>
+        </div>
+        {/* 2 */}
+        <div className="destaque-card">
+          <img
+            src="https://via.placeholder.com/300x200"
+            alt="Olimpíada de Astronomia"
+            className="destaque-image"
+          />
+          <div className="destaque-info">
+            <h3>
+              <i className="fa-solid fa-medal medalha-prata"></i> Olimpíada de Astronomia
+            </h3>
+            <p>
+              Maria Souza foi a grande vencedora da Olimpíada Brasileira de
+              Astronomia e Astronáutica, representando nossa escola com grande
+              brilho e paixão pela ciência.
+            </p>
+          </div>
+        </div>
+        {/* 3 */}
+        <div className="destaque-card">
+          <img
+            src="https://via.placeholder.com/300x200"
+            alt="Olimpíada de Redação"
+            className="destaque-image"
+          />
+          <div className="destaque-info">
+            <h3>
+              <i className="fa-solid fa-medal medalha-bronze"></i> Olimpíada de Redação
+            </h3>
+            <p>
+              A aluna Ana Costa conquistou o primeiro lugar na Olimpíada Nacional
+              de Redação, com seu texto premiado abordando temas de relevância
+              social.
+            </p>
+          </div>
+        </div>
+
+        
       </div>
     </section>
   );
